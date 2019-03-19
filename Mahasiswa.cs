8a9d9d04c364f14a42f859b4197d92d2e0713d84
Qiraht <thariq2712@gmail.com>
2019-03-19 12:10:51 +0700

    Pemrograman C# menggunakan Class dan Object

---
+++ Mahasiswa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_Class
+{
+    public class Mahasiswa
+    {
+        public string NIM { get; set; }
+        public string Nama { get; set; }
+        public float IpK { get; set; }
+
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama : {0}", Nama);
+            Console.WriteLine("NIM : {0}", NIM);
+            Console.WriteLine("IPK : {0}", IpK);
+            Console.WriteLine("Telah Melakukan Registrasi");
+        }
+
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses Krs\n", Nama);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            saya.NIM = "18.11.2350";
+            saya.Nama = "Thariq";
+            saya.IpK = 4f;
+
+            kamu.NIM = "19.2666";
+            kamu.Nama = "BOT";
+            kamu.IpK = 4f;
+
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
